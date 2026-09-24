b1e03a935e83dce452026794b1ff37b692cb9954
Gabriel <gabrielmn.machadonunes@gmail.com>
2026-09-23 21:53:42 -0300

    first CSES problem

---
+++ CSES/Introductory-Problems/weird_algorithm_gmn.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long int n;
+
+    cin >> n;
+
+    
+    while(n != 1){
+        
+        cout << n << " ";
+        
+        // n is even
+        if(n % 2 == 0){
+            n /= 2;
+        }
+        // n is odd
+        else{          
+            n =  n * 3 + 1;
+        }
+    }
+
+    cout << "1" << endl;
+
+
+    return 0;
+}
+
